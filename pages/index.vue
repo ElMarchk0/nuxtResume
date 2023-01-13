bd2031383f528ac0c4e133a0fbeb8fbb275cdfd3
EMarckh0 <petermc9011@gmail.com>
2023-01-12 16:17:07 -0800

    minor updates

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,19 @@
   <NuxtLayout name="default">
     <div class="flex justify-center flex-col md:justify-start">
       <h1 class="text-center text-4xl">Welcome To My Online Resume</h1>
-      <p class="text-center py-16">
+      <p class="text-center p-16">
         Hi, my name is Peter Charlesworth and I am aspiring software developer
-        based in Victoria, British Columbia.
+        based in Victoria, British Columbia. Checkout my
+        <span class="text-blue-400"
+          ><a href="https://github.com/ElMarchk0">Github</a></span
+        >
+        and
+        <span class="text-blue-400"
+          ><a href="https://www.linkedin.com/in/peter-charlesworth-22b866215/"
+            >LinkedIn</a
+          ></span
+        >
+        for more information.
       </p>
     </div>
   </NuxtLayout>
